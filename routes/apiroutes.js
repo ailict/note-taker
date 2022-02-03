25e3672911d06a0a39dae5c129e0403297fdeeaf
ailict <ailichatfieldtaylor@gmail.com>
2022-02-02 20:13:43 -0500

    set up api route for get notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,1 +1,12 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+//need a get 
+router.get('/notes', (req, res) => {
+    let data = fs.readFileSync("./db/db.json");
+    let notes = JSON.parse(data);
+    res.json(notes);
+});
+
+//need a post route that gets req . body and writes into db . jso
